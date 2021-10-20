7bf223b097184dc7f55e7280b17a372292050120
tyanotyrano <tyrano09@gmail.com>
2021-10-20 22:08:42 +0900

    feat(SampleRestController.java): GET 방식의 Rest Api 추가
    GET 방식의 Rest Api 를 Query Parameter, Path Variable 방식으로 추가한다.
    - readByQueryParameter() : Query Parameter 방식의 GET Rest Api
    - readByPathVariable() : Path Variable 방식의 GET Rest Api

--- src/main/java/com/tyranotyrano/swaggerlearning/web/controller/SampleRestController.java
+++ src/main/java/com/tyranotyrano/swaggerlearning/web/controller/SampleRestController.java
@@ -1,16 +1,39 @@
 package com.tyranotyrano.swaggerlearning.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
 public class SampleRestController {
 
-	@ApiOperation("Sample Rest Api 입니다.")
+	@ApiOperation("Query Parameter 를 사용한 API")
+	@ApiImplicitParam(
+		name = "id",
+		dataType = "long",
+		required = true,
+		paramType = "query",
+		defaultValue = "1",
+		value = "Query Parameter 로 받은 ID")
 	@GetMapping("/tmp")
-	public String tmp() {
-		return "Swagger Test Success";
+	public String readByQueryParameter(@RequestParam(value = "id") Long id) {
+		return "[GET] Read With Query Parameter. id = " + id;
+	}
+
+	@ApiOperation("Path Variable 을 사용한 API")
+	@ApiImplicitParam(
+		name = "id",
+		dataType = "long",
+		required = true,
+		paramType = "path",
+		defaultValue = "1",
+		value = "Path Variable 로 받은 ID")
+	@GetMapping("/tmp/{id}")
+	public String readByPathVariable(@PathVariable("id") Long id) {
+		return "[GET] Read With Path Variable. id = " + id;
 	}
 }
